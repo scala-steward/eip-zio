d123275a95a839e6d1f6f788d86928c42c94818a
Paul Campbell <pcampbell@kemitix.net>
2019-08-21 22:51:46 +0100

    Add support for enviRonment types in sender and receivers (#2)
    * Add support for ZIO Environments in sender and receiver
    
    * Add .travis.yml
    
    * Don't build for 2.12.8

--- src/main/scala/net/kemitix/eip/zio/MessageChannel.scala
+++ src/main/scala/net/kemitix/eip/zio/MessageChannel.scala
@@ -1,15 +1,22 @@
 package net.kemitix.eip.zio
 
-import zio.{IO, UIO, ZIO, stream}
+import zio.{IO, RIO, UIO, ZIO, stream}
 
 object MessageChannel {
-  type Channel           = zio.stream.Stream[Unit, Unit]
-  type Callback[Message] = IO[Option[Unit], Message] => Unit
-  type Sender[Message]   = Callback[Message] => UIO[Unit]
-  type Receiver[Message] = Message => UIO[Unit]
+  type Callback[Message]     = IO[Option[Unit], Message] => Unit
+  type Channel               = zio.stream.Stream[Unit, Unit]
+  type Sender[Message]       = Callback[Message] => UIO[Unit]
+  type Receiver[Message]     = Message => UIO[Unit]
+  type CallbackR[R, Message] = ZIO[R, Option[Unit], Message] => Unit
+  type ChannelR[R]           = zio.stream.ZStream[R, Unit, Unit]
+  type SenderR[R, Message]   = CallbackR[R, Message] => ZIO[R, Nothing, Unit]
+  type ReceiverR[R, Message] = Message => ZIO[R, Nothing, Unit]
   def endChannel[Message]: Callback[Message] => Unit =
     cb => cb(ZIO.fail(None))
   def send[Message](cb: Callback[Message])(message: Message): Unit =
+    cb(ZIO.succeed(message))
+  def sendR[RSend, Message](cb: CallbackR[RSend, Message])(
+      message: Message): Unit =
     cb(ZIO.succeed(message))
 
   /**
@@ -31,8 +38,15 @@ object MessageChannel {
     * @param sender A callback that returns an effect
     * @param receiver A function that will be called for each Message
     * @tparam Message The type of the Message
+    * @tparam RSend The environment for the sender
+    * @tparam RReceive The environment for the receiver
     * @return a Channel
     */
+  def pointToPointR[RSend, RReceive, Message](sender: SenderR[RSend, Message])(
+      receiver: ReceiverR[RReceive, Message]
+  ): ChannelR[RSend with RReceive] = {
+    pointToPointRPar[RSend, RReceive, Message](1)(sender)(receiver)
+  }
   def pointToPoint[Message](sender: Sender[Message])(
       receiver: Receiver[Message]
   ): Channel = {
@@ -40,16 +54,24 @@ object MessageChannel {
   }
 
   /**
-    * See [[MessageChannel.pointToPoint()]]
+    * See [[MessageChannel.pointToPointR()]]
     *
     * Executes 'n' receivers running in parallel.
     *
     * @param n the number of parallel receivers
     */
+  def pointToPointRPar[RSend, RReceive, Message](n: Int)(
+      sender: SenderR[RSend, Message])(
+      receiver: ReceiverR[RReceive, Message]
+  ): ChannelR[RSend with RReceive] = {
+    zio.stream.ZStream
+      .effectAsyncM((cb: CallbackR[RSend, Message]) => sender(cb).fork.unit)
+      .mapMPar(n)(receiver)
+  }
   def pointToPointPar[Message](n: Int)(sender: Sender[Message])(
       receiver: Receiver[Message]
   ): Channel = {
-    zio.stream.Stream
+    zio.stream.ZStream
       .effectAsyncM((cb: Callback[Message]) => sender(cb).fork.unit)
       .mapMPar(n)(receiver)
   }
--- src/test/scala/net/kemitix/eip/zio/MessageChannelTest.scala
+++ src/test/scala/net/kemitix/eip/zio/MessageChannelTest.scala
@@ -4,6 +4,7 @@ import java.util.concurrent.CountDownLatch
 import java.util.concurrent.atomic.AtomicReference
 
 import org.scalatest.FreeSpec
+import zio.console.Console
 import zio.{DefaultRuntime, UIO}
 
 class MessageChannelTest extends FreeSpec {
@@ -83,6 +84,88 @@ class MessageChannelTest extends FreeSpec {
         assert(
           output.get.reverse
             .containsSlice(List("finished 3", "finished 2", "finished 1")))
+      }
+    }
+    "with Environments" - {
+      "receive messages asynchronously" in {
+        val output = new AtomicReference[List[String]](List.empty[String])
+        val latch  = new AtomicReference[CountDownLatch](new CountDownLatch(1))
+
+        def putStr(s: String): Unit = {
+          println(s)
+          output.updateAndGet(l => s :: l)
+        }
+
+        def sender: MessageChannel.SenderR[Console, Int] =
+          cb =>
+            UIO {
+              (1 to 3).foreach { message =>
+                putStr(s"put $message")
+                // use of latch is only to avoid tests being flaky
+                // don't use them in your own code
+                latch.set(new CountDownLatch(1))
+                MessageChannel.sendR(cb)(message)
+                // ensure receiver has completed:
+                latch.get.await()
+              }
+              MessageChannel.endChannel(cb)
+          }
+
+        def receiver: MessageChannel.ReceiverR[Console, Int] =
+          message =>
+            UIO {
+              putStr(s"got $message")
+              latch.get.countDown()
+          }
+
+        val program =
+          MessageChannel.pointToPointR[Console, Console, Int](sender)(receiver)
+        new DefaultRuntime {}
+          .unsafeRunSync(program.runDrain.provide(Console.Live))
+        val expected = List(
+          "put 1",
+          "got 1",
+          "put 2",
+          "got 2",
+          "put 3",
+          "got 3"
+        )
+        assertResult(expected)(output.get.reverse)
+      }
+      "multiple receivers" - {
+        "consumed once and non-blocking" in {
+          // messages are only processed by one receiver
+          // messages being processed don't block other receivers
+          val output = new AtomicReference[List[String]](List.empty[String])
+
+          def putStr(s: String): Unit = {
+            println(s)
+            output.updateAndGet(l => s :: l)
+          }
+
+          val nFibers = 3
+          def sender: MessageChannel.SenderR[Console, Int] =
+            cb =>
+              UIO {
+                (1 to nFibers).foreach(MessageChannel.sendR(cb))
+                MessageChannel.endChannel(cb)
+            }
+
+          def receiver: MessageChannel.ReceiverR[Console, Int] =
+            message =>
+              UIO {
+                Thread.sleep((nFibers - message) * 10) // reverse the order of completion
+                putStr(s"finished $message")
+            }
+
+          val program =
+            MessageChannel.pointToPointRPar(nFibers)(sender)(receiver)
+          new DefaultRuntime {}
+            .unsafeRunSync(program.runDrain.provide(Console.Live))
+          assert(
+            output.get.reverse
+              .containsSlice(List("finished 3", "finished 2", "finished 1")))
+        }
       }
     }
   }
