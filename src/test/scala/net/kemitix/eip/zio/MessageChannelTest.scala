d827fcacaebe8e244d10b8abb2a461d55620794e
Paul Campbell <pcampbell@kemitix.net>
2019-08-22 22:50:51 +0100

    [MessageChannelTest] add some usecase tests

--- src/test/scala/net/kemitix/eip/zio/MessageChannelTest.scala
+++ src/test/scala/net/kemitix/eip/zio/MessageChannelTest.scala
@@ -5,6 +5,7 @@ import java.util.concurrent.atomic.AtomicReference
 
 import net.kemitix.eip.zio.MessageChannel.ChannelHandle
 import org.scalatest.FreeSpec
+import org.scalatest.Matchers._
 import zio.clock.Clock
 import zio.console._
 import zio.{DefaultRuntime, UIO, ZIO}
@@ -18,7 +19,7 @@ class MessageChannelTest extends FreeSpec {
 
       def putStr(s: String): ZIO[Console, Nothing, Unit] =
         for {
-          _ <- zio.console.putStrLn(s)
+          _ <- putStrLn(s)
           _ <- UIO(output.updateAndGet(l => s :: l))
         } yield ()
 
@@ -71,11 +72,12 @@ class MessageChannelTest extends FreeSpec {
 
         def putStr(s: String): ZIO[Console, Nothing, Unit] =
           for {
-            _ <- zio.console.putStrLn(s)
+            _ <- putStrLn(s)
             _ <- UIO(output.updateAndGet(l => s :: l))
           } yield ()
 
         val nFibers = 3
+
         def sender: MessageChannel.Sender[Clock, Int] =
           channel =>
             for {
@@ -114,6 +116,105 @@ class MessageChannelTest extends FreeSpec {
         assert(
           output.get.reverse
             .containsSlice(List("finished 3", "finished 2", "finished 1")))
+      }
+    }
+    "use cases" - {
+      val output = new AtomicReference[List[String]](List.empty[String])
+
+      def putStr(s: String): ZIO[Console, Nothing, Unit] =
+        for {
+          _ <- putStrLn(s)
+          _ <- UIO(output.updateAndGet(l => s :: l))
+        } yield ()
+
+      def sender(id: String): MessageChannel.Sender[Clock with Console, Int] =
+        channel =>
+          for {
+            _ <- ZIO.foreach(1 to 3) { counter =>
+              for {
+                message <- Message.create(counter)
+                _       <- putStr(s"sender $id sending: " + message.body.toString)
+                _       <- MessageChannel.send(channel)(message)
+              } yield ()
+            }
+            _ <- MessageChannel.endChannel(channel)
+          } yield ()
+
+      def receiver(id: String): MessageChannel.Receiver[Console, Int] =
+        message =>
+          for {
+            _ <- putStr(s"receiver $id received: " + message.body.toString)
+          } yield ()
+
+      "start second channel when first closes" in {
+        //given
+        val channel1 =
+          MessageChannel.pointToPoint(sender("first"))(receiver("first"))
+        val channel2 =
+          MessageChannel.pointToPoint(sender("second"))(receiver("second"))
+        //when
+        val program: ZIO[Clock with Console, Unit, Unit] =
+          channel1.runDrain *> channel2.runDrain
+        //then
+        new DefaultRuntime {}.unsafeRunSync(program)
+        val expectedFirst = List(
+          "sender first sending: 1",
+          "sender first sending: 2",
+          "sender first sending: 3",
+          "receiver first received: 1",
+          "receiver first received: 2",
+          "receiver first received: 3"
+        )
+        val expectedSecond = List(
+          "sender second sending: 1",
+          "sender second sending: 2",
+          "sender second sending: 3",
+          "receiver second received: 1",
+          "receiver second received: 2",
+          "receiver second received: 3"
+        )
+        val (first, second) = output.get.reverse.splitAt(6)
+        first should contain allElementsOf (expectedFirst)
+        second should contain allElementsOf (expectedSecond)
+      }
+      "start third channel when two others both close" in {
+        //given
+        val channel1 =
+          MessageChannel.pointToPoint(sender("first"))(receiver("first"))
+        val channel2 =
+          MessageChannel.pointToPoint(sender("second"))(receiver("second"))
+        val channel3 =
+          MessageChannel.pointToPoint(sender("third"))(receiver("third"))
+        //when
+        val program: ZIO[Clock with Console, Unit, Unit] =
+          (channel1.runDrain <&> channel2.runDrain) *> channel3.runDrain
+        //then
+        new DefaultRuntime {}.unsafeRunSync(program)
+        val expectedFirst = List(
+          "sender first sending: 1",
+          "sender first sending: 2",
+          "sender first sending: 3",
+          "receiver first received: 1",
+          "receiver first received: 2",
+          "receiver first received: 3",
+          "sender second sending: 1",
+          "sender second sending: 2",
+          "sender second sending: 3",
+          "receiver second received: 1",
+          "receiver second received: 2",
+          "receiver second received: 3"
+        )
+        val expectedSecond = List(
+          "sender third sending: 1",
+          "sender third sending: 2",
+          "sender third sending: 3",
+          "receiver third received: 1",
+          "receiver third received: 2",
+          "receiver third received: 3"
+        )
+        val (first, second) = output.get.reverse.splitAt(12)
+        first should contain allElementsOf (expectedFirst)
+        second should contain allElementsOf (expectedSecond)
       }
     }
   }
